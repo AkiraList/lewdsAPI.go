df07ca91cb9ceaba8230deb73356215fb838e27a
Xynnix <zkjcksn@gmail.com>
2021-09-03 21:58:00 -0400

    2nd Commit?

--- endpoints.go
+++ endpoints.go
@@ -6,110 +6,110 @@ import (
 
 func (client *Client) ass(image url.URL) (interface{}, error) {
 	return client.MakeRequest("ass", url.Values{
-		"result": {result.String()},
+		"result": {image.String()},
 	})
 }
 func (client *Client) athighs(image url.URL) (interface{}, error) {
 	return client.MakeRequest("athighs", url.Values{
-		"result": {result.String()},
+		"result": {image.String()},
 	})
 }
 
 func (client *Client) blow(image url.URL) (interface{}, error) {
 	return client.MakeRequest("blow", url.Values{
-		"result": {result.String()},
+		"result": {image.String()},
 	})
 }
 func (client *Client) boobs(image url.URL) (interface{}, error) {
 	return client.MakeRequest("boobs", url.Values{
-		"result": {result.String()},
+		"result": {image.String()},
 	})
 }
 
 func (client *Client) feet(image url.URL) (interface{}, error) {
 	return client.MakeRequest("feet", url.Values{
-		"result": {result.String()},
+		"result": {image.String()},
 	})
 }
 func (client *Client) furfuta(image url.URL) (interface{}, error) {
 	return client.MakeRequest("furfuta", url.Values{
-		"result": {result.String()},
+		"result": {image.String()},
 	})
 }
 
 func (client *Client) furgif(image url.URL) (interface{}, error) {
 	return client.MakeRequest("furgif", url.Values{
-		"result": {result.String()},
+		"result": {image.String()},
 	})
 }
 func (client *Client) futa(image url.URL) (interface{}, error) {
 	return client.MakeRequest("futa", url.Values{
-		"result": {result.String()},
+		"result": {image.String()},
 	})
 }
 
 func (client *Client) gifs(image url.URL) (interface{}, error) {
 	return client.MakeRequest("gifs", url.Values{
-		"result": {result.String()},
+		"result": {image.String()},
 	})
 }
 func (client *Client) hboobs(image url.URL) (interface{}, error) {
 	return client.MakeRequest("hboobs", url.Values{
-		"result": {result.String()},
+		"result": {image.String()},
 	})
 }
 
 func (client *Client) hentai(image url.URL) (interface{}, error) {
 	return client.MakeRequest("hentai", url.Values{
-		"result": {result.String()},
+		"result": {image.String()},
 	})
 }
 func (client *Client) hfeet(image url.URL) (interface{}, error) {
 	return client.MakeRequest("hfeet", url.Values{
-		"result": {result.String()},
+		"result": {image.String()},
 	})
 }
 
 func (client *Client) kink(image url.URL) (interface{}, error) {
 	return client.MakeRequest("kink", url.Values{
-		"result": {result.String()},
+		"result": {image.String()},
 	})
 }
 func (client *Client) milk(image url.URL) (interface{}, error) {
 	return client.MakeRequest("milk", url.Values{
-		"result": {result.String()},
+		"result": {image.String()},
 	})
 }
 
 func (client *Client) pantsu(image url.URL) (interface{}, error) {
 	return client.MakeRequest("pantsu", url.Values{
-		"result": {result.String()},
+		"result": {image.String()},
 	})
 }
 func (client *Client) sex(image url.URL) (interface{}, error) {
 	return client.MakeRequest("sex", url.Values{
-		"result": {result.String()},
+		"result": {image.String()},
 	})
 }
 
 func (client *Client) slime(image url.URL) (interface{}, error) {
 	return client.MakeRequest("slime", url.Values{
-		"result": {result.String()},
+		"result": {image.String()},
 	})
 }
 func (client *Client) thighs(image url.URL) (interface{}, error) {
 	return client.MakeRequest("thighs", url.Values{
-		"result": {result.String()},
+		"result": {image.String()},
 	})
 }
 
 func (client *Client) trap(image url.URL) (interface{}, error) {
 	return client.MakeRequest("trap", url.Values{
-		"result": {result.String()},
+		"result": {image.String()},
 	})
 }
 func (client *Client) yuri(image url.URL) (interface{}, error) {
 	return client.MakeRequest("yuri", url.Values{
-		"result": {result.String()},
+		"result": {image.String()},
 	})
 }
